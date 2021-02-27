d6609710f1b706dd09ba78dace1d479f6902a06a
Laura Ramirez <lauramirez0320@gmail.com>
2021-02-27 18:28:23 -0500

    Modularización de las funciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+var creditCardNumber = document.getElementById('dnumber').value;
+
+document.getElementById('dnumber').addEventListener("input", function(event) {
+    let input = event.target.value;
+    creditCardNumber = creditCardNumber + input.slice(-1);
+    let klcfb = validator.maskify(input);
+    document.getElementById('dnumber').value=klcfb;
+    
+    console.log(klcfb)
+});
+ 
+
+let erdf = document.getElementById('confirm').addEventListener("click", function() {validator.isValid(creditCardNumber)});
+
+
+
+
+         
+
+
+  
+
+
+
+
+
